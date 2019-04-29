31cac75daf73f91b4e5ce068545cadf566538538
userzhangjinlong <825188432@qq.com>
2019-04-29 17:56:52 +0800

    前段文件构建

--- resources/views/layouts/admin-head.blade.php
+++ resources/views/layouts/admin-head.blade.php
@@ -12,35 +12,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
-    <style>
-        /*控制菜单箭头*/
-        .nav-header.collapsed > span.glyphicon-chevron-toggle:before {
-            content: "\e114";
-        }
-
-        .nav-header > span.glyphicon-chevron-toggle:before {
-            content: "\e113";
-        }
-        .secondmenu a {
-            font-size: 12px;
-            color: #4A515B;
-            text-align: center;
-        }
-
-        .secondmenu li.active {
-            background-color: #eee;
-            border-color: #428bca;
-        }
-        .nav-header.collapsed > span.glyphicon-chevron-toggle:before {
-            content: "\e114";
-        }
-
-        .nav-header > span.glyphicon-chevron-toggle:before {
-            content: "\e113";
-        }
-
-    </style>
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet">
 
 </head>
 <body>
@@ -98,80 +70,80 @@
         </nav>
         <nav class="navbar-left" style="width: 16%;float: left">
             {{--<div class="row">--}}
-                <div class="col-md-2" style="width: 100%;">
-                    <ul id="main-nav" class="main-nav nav nav-tabs nav-stacked" style="">
-                        {{--<li>--}}
-                            {{--<a href="#">--}}
-                                {{--<i class="glyphicon glyphicon-th-large"></i>--}}
-                                {{--首页--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        <li>
-                            <a href="#systemSetting" class="nav-header collapsed" data-toggle="collapse">
-                                <i class="glyphicon glyphicon-search"></i>
-                                系统管理
+            <div class="col-md-2" style="width: 100%;">
+                <ul id="main-nav" class="main-nav nav nav-tabs nav-stacked" style="">
+                    {{--<li>--}}
+                    {{--<a href="#">--}}
+                    {{--<i class="glyphicon glyphicon-th-large"></i>--}}
+                    {{--首页--}}
+                    {{--</a>--}}
+                    {{--</li>--}}
+                    <li>
+                        <a href="#systemSetting" class="nav-header collapsed" data-toggle="collapse">
+                            <i class="glyphicon glyphicon-cog"></i>
+                            系统管理
 
-                                <span class="pull-right glyphicon glyphicon-chevron-toggle"></span>
-                            </a>
-                            <ul id="systemSetting" class="nav nav-list secondmenu collapse" style="height: 0px;">
-                                <li><a href="#"><i class="glyphicon glyphicon-user"></i>&nbsp;用户管理</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>&nbsp;菜单管理</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-asterisk"></i>&nbsp;角色管理</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-edit"></i>&nbsp;修改密码</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-eye-open"></i>&nbsp;日志查看</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#configSetting" class="nav-header collapsed" data-toggle="collapse">
-                                <i class="glyphicon glyphicon-credit-card"></i>
-                                配置管理
-                                <span class="pull-right glyphicon  glyphicon-chevron-toggle"></span>
-                            </a>
-                            <ul id="configSetting" class="nav nav-list secondmenu collapse in">
-                                <li class="active"><a href="#"><i class="glyphicon glyphicon-globe"></i>&nbsp;全局缺省配置</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-star-empty"></i>&nbsp;未开通用户配置</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-star"></i>&nbsp;退订用户配置</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-text-width"></i>&nbsp;试用用户配置</a></li>
-                                <li><a href="#"><i class="glyphicon glyphicon-ok-circle"></i>&nbsp;开通用户配置</a></li>
-                            </ul>
-                        </li>
+                            <span class="pull-right glyphicon glyphicon-plus"></span>
+                        </a>
+                        <ul id="systemSetting" class="nav nav-list secondmenu collapse" style="height: 0px;">
+                            <li><a href="#"><i class="glyphicon glyphicon-user"></i>&nbsp;用户管理</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>&nbsp;菜单管理</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-asterisk"></i>&nbsp;角色管理</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-edit"></i>&nbsp;修改密码</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-eye-open"></i>&nbsp;日志查看</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#configSetting" class="nav-header collapsed" data-toggle="collapse">
+                            <i class="glyphicon glyphicon-credit-card"></i>
+                            配置管理
+                            <span class="pull-right glyphicon  glyphicon-chevron-toggle"></span>
+                        </a>
+                        <ul id="configSetting" class="nav nav-list secondmenu collapse in">
+                            <li class="active"><a href="#"><i class="glyphicon glyphicon-globe"></i>&nbsp;全局缺省配置</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-star-empty"></i>&nbsp;未开通用户配置</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-star"></i>&nbsp;退订用户配置</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-text-width"></i>&nbsp;试用用户配置</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-ok-circle"></i>&nbsp;开通用户配置</a></li>
+                        </ul>
+                    </li>
 
-                        <li>
-                            <a href="#disSetting" class="nav-header collapsed" data-toggle="collapse">
-                                <i class="glyphicon glyphicon-globe"></i>
-                                分发配置
-                                <span class="pull-right glyphicon glyphicon-chevron-toggle"></span>
-                            </a>
-                            <ul id="disSetting" class="nav nav-list secondmenu collapse">
-                                <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>&nbsp;分发包配置</a></li>
-                            </ul>
-                        </li>
+                    <li>
+                        <a href="#disSetting" class="nav-header collapsed" data-toggle="collapse">
+                            <i class="glyphicon glyphicon-globe"></i>
+                            分发配置
+                            <span class="pull-right glyphicon glyphicon-chevron-toggle"></span>
+                        </a>
+                        <ul id="disSetting" class="nav nav-list secondmenu collapse">
+                            <li><a href="#"><i class="glyphicon glyphicon-th-list"></i>&nbsp;分发包配置</a></li>
+                        </ul>
+                    </li>
 
-                        <li>
-                            <a href="#dicSetting" class="nav-header collapsed" data-toggle="collapse">
-                                <i class="glyphicon glyphicon-bold"></i>
-                                字典配置
-                                <span class="pull-right glyphicon glyphicon-chevron-toggle"></span>
-                            </a>
-                            <ul id="dicSetting" class="nav nav-list secondmenu collapse">
-                                <li><a href="#"><i class="glyphicon glyphicon-text-width"></i>&nbsp;关键字配置</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <i class="glyphicon glyphicon-fire"></i>
-                                关于系统
-                                <span class="badge pull-right">1</span>
-                            </a>
-                        </li>
+                    <li>
+                        <a href="#dicSetting" class="nav-header collapsed" data-toggle="collapse">
+                            <i class="glyphicon glyphicon-bold"></i>
+                            字典配置
+                            <span class="pull-right glyphicon glyphicon-chevron-toggle"></span>
+                        </a>
+                        <ul id="dicSetting" class="nav nav-list secondmenu collapse">
+                            <li><a href="#"><i class="glyphicon glyphicon-text-width"></i>&nbsp;关键字配置</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i class="glyphicon glyphicon-fire"></i>
+                            关于系统
+                            <span class="badge pull-right">1</span>
+                        </a>
+                    </li>
 
-                    </ul>
-                </div>
+                </ul>
+            </div>
             {{--</div>--}}
         </nav>
 
 
-                @yield('content')
+        @yield('content')
     </div>
 
     <!-- Scripts -->
